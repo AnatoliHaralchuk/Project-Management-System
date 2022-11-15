769e8b4cde0fbf8ee76f2f802179159aeab7f1b3
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-11-15 04:07:22 -0800

    feat: add carusel comp and done welcome page

--- src/app/project-management/project-management.module.ts
+++ src/app/project-management/project-management.module.ts
@@ -1,13 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CaruselComponent } from './components/carusel/carusel.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [
+      { path: '', redirectTo: '/main', pathMatch: 'full' },
+      { path: 'main', component: MainPageComponent },
+    ],
+  },
+];
 @NgModule({
   declarations: [
     WelcomePageComponent,
     MainPageComponent,
+    CaruselComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  exports: [RouterModule]
 })
 export class ProjectManagementModule {}
