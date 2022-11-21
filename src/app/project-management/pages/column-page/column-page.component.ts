ba1cb96e3ec364e9f31b64bf86b935d49c4df1a2
AnatoliHaralchuk <sshades@mail.ru>
2022-11-21 16:57:01 +0300

    feat: add board page

---
+++ src/app/project-management/pages/column-page/column-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-page',
+  templateUrl: './column-page.component.html',
+  styleUrls: ['./column-page.component.scss']
+})
+export class ColumnPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/project-management/project-management.module.ts
+++ src/app/project-management/project-management.module.ts
@@ -9,17 +9,29 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CaruselComponent } from './components/carusel/carusel.component';
 import { AuthGuard } from '../auth/auth.guard';
+import { BoardComponent } from './components/board/board.component';
+import { ColumnComponent } from './components/column/column.component';
+import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { ColumnPageComponent } from './pages/column-page/column-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomePageComponent },
-  { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },
+  { path: 'main', component: MainPageComponent, canActivate: [AuthGuard], children: [
+      { path: '', redirectTo: '/main/board', pathMatch: 'full' },
+      { path:'board', component: BoardPageComponent },
+      { path: 'board/column', component: ColumnPageComponent },
+    ] },
 ];
 @NgModule({
   declarations: [
     WelcomePageComponent,
     MainPageComponent,
     CaruselComponent,
+    BoardComponent,
+    ColumnComponent,
+    BoardPageComponent,
+    ColumnPageComponent,
   ],
   imports: [
     RouterModule.forChild(routes),
